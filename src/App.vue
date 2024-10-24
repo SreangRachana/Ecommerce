1318cc7062ba5d6314619cb52f30aa55309b83d6
SreangRachana <163454558+SreangRachana@users.noreply.github.com>
2024-10-24 09:22:15 +0700

    Finish step 3 in TP1
      * Clean unused code

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  
+    <div>
+      This is my first  project
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
